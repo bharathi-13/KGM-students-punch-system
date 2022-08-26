7ddeb83523e0c744356bd93395541626a530620a
bharathi-13 <sathyabharathi00131@gmail.com>
2022-08-26 07:38:30 +0530

    I did some link redirection

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
-def message():
-    return "<h1>Welcome</h1>"
+@app.route("/login")
+def login():
+    return "<h1>Login page</h1>"
+
+@app.route("/home")
+def home():
+    return "<h1>Home page</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
