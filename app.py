455875b718d38f31659f0b198780621936244432
bharathi-13 <sathyabharathi00131@gmail.com>
2022-08-26 21:02:47 +0530

    Did some basic page layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/login")
 def login():
-    return "<h1>Login page</h1>"
+    return render_template('login.html')
 
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
